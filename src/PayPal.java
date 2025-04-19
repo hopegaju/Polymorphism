ff1a5a8e46dcdf2acf957843209b2f515523d127
hopegaju <hopegajupr@gmail.com>
2025-04-19 15:26:15 +0200

    Added paypal credentials

--- src/PayPal.java
+++ src/PayPal.java
@@ -1,2 +1,17 @@
 public class PayPal {
+    private String email;
+    private int password;
+
+    public PayPal(String email, int password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPassword() {
+        return password;
+    }
 }
