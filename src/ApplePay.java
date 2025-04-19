03bafcf0186d77fb9a9bd896245dc67e5bcf47c9
hopegaju <hopegajupr@gmail.com>
2025-04-19 16:38:36 +0200

    apple pay credentials

--- src/ApplePay.java
+++ src/ApplePay.java
@@ -1,3 +1,16 @@
 public class ApplePay {
+    private String accountNumber;
+    private String authentication;
+    public ApplePay(String accountNumber, String authentication) {
+        this.accountNumber = accountNumber;
+        this.authentication = authentication;
+    }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAuthentication() {
+        return authentication;
+    }
 }
